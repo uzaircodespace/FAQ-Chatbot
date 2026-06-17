5f580043ce163c80f639f6dbcc26ddeb3f5d7684
Uzair Ahmad Khan <nishatschool55-boop>
2026-06-17 17:27:01 +0500

    fix nltk dependency

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-import json
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')import json
 import string
 import nltk
 from flask import Flask, render_template, request, jsonify
@@ -65,3 +70,8 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
